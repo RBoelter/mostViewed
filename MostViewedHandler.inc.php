76cf2ecb5466e5196abf2b70202e0555e9a8ca4b
RBoelter <ronny.boelter@outlook.com>
2020-03-12 13:31:24 +0100

    plugin rebuild

--- MostViewedHandler.inc.php
+++ MostViewedHandler.inc.php
@@ -22,48 +22,54 @@ class MostViewedHandler extends ScheduledTask
 		if (!$plugin->getEnabled()) {
 			return false;
 		}
+
 		$mostReadDays = 30;
 		$range = 5;
 		$date = null;
-		$contextId = Application::getRequest()->getContext()->getId();
-		$publishedArticleDao = DAORegistry::getDAO('PublishedArticleDAO');
-		$journalDao = DAORegistry::getDAO('JournalDAO');
-		$dayString = "-" . $mostReadDays . " days";
-		$daysAgo = date('Ymd', strtotime($dayString));
-		$currentDate = date('Ymd');
-		$filter = array(
-			STATISTICS_DIMENSION_CONTEXT_ID => $contextId,
-		);
-		$filter[STATISTICS_DIMENSION_DAY]['from'] = $daysAgo;
-		$filter[STATISTICS_DIMENSION_DAY]['to'] = $currentDate;
-		$orderBy = array(STATISTICS_METRIC => STATISTICS_ORDER_DESC);
-		$column = array(STATISTICS_DIMENSION_SUBMISSION_ID);
-		import('lib.pkp.classes.db.DBResultRange');
-		$dbResultRange = new DBResultRange($date ? null : $range + 1);
-		$metricsDao =& DAORegistry::getDAO('MetricsDAO');
-		$result = $metricsDao->getMetrics(OJS_METRIC_TYPE_COUNTER, $column, $filter, $orderBy, $dbResultRange);
-		$articles = array();
-		$cc = 0;
-		foreach ($result as $resultRecord) {
-			$submissionId = $resultRecord[STATISTICS_DIMENSION_SUBMISSION_ID];
-			$article = $publishedArticleDao->getById($submissionId);
-			if ($article) {
-				if ($date && $article->getDatePublished() < $date)
-					continue;
-				$journal = $journalDao->getById($article->getJournalId());
-				$articles[$submissionId]['journalPath'] = $journal->getPath();
-				$articles[$submissionId]['articleId'] = $article->getBestArticleId();
-				$articles[$submissionId]['articleTitle'] = $article->getLocalizedTitle($article->getLocale());
-				$articles[$submissionId]['articleSubtitle'] = $article->getLocalizedSubtitle($article->getLocale());
-				$articles[$submissionId]['articleAuthor'] = $article->getAuthorString();
-				$articles[$submissionId]['metric'] = $resultRecord[STATISTICS_METRIC];
-				if (++$cc >= $range)
-					break;
+
+		$application = Application::getApplication();
+		$contextDao = $application->getContextDAO();
+		for ($contexts = $contextDao->getAll(true); $context = $contexts->next(); ) {
+			$contextId = $context->getId();
+			$publishedArticleDao = DAORegistry::getDAO('PublishedArticleDAO');
+			$journalDao = DAORegistry::getDAO('JournalDAO');
+			$dayString = "-" . $mostReadDays . " days";
+			$daysAgo = date('Ymd', strtotime($dayString));
+			$currentDate = date('Ymd');
+			$filter = array(
+				STATISTICS_DIMENSION_CONTEXT_ID => $contextId,
+			);
+			$filter[STATISTICS_DIMENSION_DAY]['from'] = $daysAgo;
+			$filter[STATISTICS_DIMENSION_DAY]['to'] = $currentDate;
+			$orderBy = array(STATISTICS_METRIC => STATISTICS_ORDER_DESC);
+			$column = array(STATISTICS_DIMENSION_SUBMISSION_ID);
+			import('lib.pkp.classes.db.DBResultRange');
+			$dbResultRange = new DBResultRange($date ? null : $range + 1);
+			$metricsDao =& DAORegistry::getDAO('MetricsDAO');
+			$result = $metricsDao->getMetrics(OJS_METRIC_TYPE_COUNTER, $column, $filter, $orderBy, $dbResultRange);
+			$articles = array();
+			$cc = 0;
+			foreach ($result as $resultRecord) {
+				$submissionId = $resultRecord[STATISTICS_DIMENSION_SUBMISSION_ID];
+				$article = $publishedArticleDao->getById($submissionId);
+				if ($article) {
+					if ($date && $article->getDatePublished() < $date)
+						continue;
+					$journal = $journalDao->getById($article->getJournalId());
+					$articles[$submissionId]['journalPath'] = $journal->getPath();
+					$articles[$submissionId]['articleId'] = $article->getBestArticleId();
+					$articles[$submissionId]['articleTitle'] = $article->getLocalizedTitle($article->getLocale());
+					$articles[$submissionId]['articleSubtitle'] = $article->getLocalizedSubtitle($article->getLocale());
+					$articles[$submissionId]['articleAuthor'] = $article->getAuthorString();
+					$articles[$submissionId]['metric'] = $resultRecord[STATISTICS_METRIC];
+					if (++$cc >= $range)
+						break;
+				}
 			}
+			$dateTime = new Datetime();
+			$plugin->updateSetting($contextId, 'articles', json_encode($articles));
+			$plugin->updateSetting($contextId, 'datetime', $dateTime->format("Y-m-d"));
 		}
-		$dateTime = new Datetime();
-		$plugin->updateSetting($contextId, 'articles', json_encode($articles));
-		$plugin->updateSetting($contextId, 'datetime', $dateTime->format("Y-m-d"));
 		return true;
 	}
 }
